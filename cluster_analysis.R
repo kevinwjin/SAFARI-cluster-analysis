150620c5063f948c6b269e5a2687e4c6f327bfde
kevinwjin <kwj200000@utdallas.edu>
2022-06-11 23:36:30 -0500

    Refactor for better variable names, improve style and readability

--- cluster_analysis.R
+++ cluster_analysis.R
@@ -44,15 +44,16 @@
 ## give low ARI) for each clustering method. This is 6 curves in total on the
 ## same plot.
 
+### PREPARATION ###
+
 # Install dependencies
-install.packages("BiocManager") # For using SAFARI
-BiocManager::install("EBImage") # For using SAFARI
+install.packages("BiocManager") # SAFARI dependency
+BiocManager::install("EBImage") # SAFARI dependency
 install.packages("remotes") # For installing SAFARI
 install_github("estfernandez/SAFARI") # SAFARI 0.1.1; newer than CRAN
 install.packages("tidyverse") # dplyr and ggplot2
 install.packages("mclust") # Hierarchical clustering and Gaussian mixture models
-install.packages("factoextra") # Visualization of clusters
-#install.packages("animation") # Visualization of clusters
+install.packages("factoextra") # Cluster analysis visualization
 
 # Load packages
 library(SAFARI) # Image processing
@@ -61,25 +62,27 @@ library(ggplot2) # Beautiful plots
 library(mclust) # Hierarchical clustering and Gaussian mixture models
 library(factoextra) # Visualization of clusters
 
-# Load images to be processed
-# setwd("C:/Users/kevin/Downloads/project/gif/_batch_test/") # Set working directory (doesn't work in Rmd)
-file.list <- dir(pattern = "gif$") # Retrieve list of all images in pwd
-#file.list.divided <- split(file.list, ceiling(seq_along(file.list)/50)) # Divide list into increments of 50
+### LOAD IMAGES ###
 
-# Identify and remove non-binary images from the file list
-is.binary <- function(img) 
+# List images to be processed (set working directory beforehand)
+file.list <- dir(pattern = "gif$") # Retrieve list of all images
+
+is_binary <- function(img) 
 {
   length(unique(c(img))) <= 2
 }
-for (file in file.list) {
-  if (is.binary(caTools::read.gif(file)$image) == FALSE) {
+
+for (file in file_list) { # Remove non-binary images from list
+  if (is_binary(caTools::read.gif(file)$image) == FALSE) {
     print(file)
-    file.list <- file.list[names(file.list) != file]
+    file_list <- file.list[names(file_list) != file]
   }
 }
 
-# Segment shape and extract 29 features of the segmented shape
-extract.parameters <- function(img) {
+### PROCESS IMAGES AND EXTRACT FEATURES ###
+
+# Segment shape and extract 29 features of segmented shape
+extract_features <- function(img) {
   this_img <- read.image(img) 
   img_segs <- binary.segmentation(this_img,
                                   id = c("NLST", "AA00474", "11030"),
@@ -88,37 +91,36 @@ extract.parameters <- function(img) {
                                   categories = c("geometric", # get all features
                                                  "boundary", 
                                                  "topological"))
-  parameters <- data.frame(img_segs[["desc"]][-c(1,1:4)]) # delete first 4 columns
-  parameters <- cbind(c(img), parameters) # add first column with file name
-  return(parameters)
+  features <- data.frame(img_segs[["desc"]][-c(1,1:4)]) # delete first 4 columns
+  features <- cbind(c(img), features) # add first column with file name
+  return(features)
 }
 
 # Extract 29 features from images
-start_time <- Sys.time() # Measure execution time
-params <- extract.parameters("apple-1.gif") # Read in first image
-for(file in file.list) { # Read in the rest of the images
-  params <- add_row(params, extract.parameters(file))
+features <- extract.features("apple-1.gif") # Read in first image
+for(file in file_list) { # Read in the rest of the images
+  features <- add_row(features, extract.features(file))
 }
-params <- params[-1, ] # Remove redundant first row (unnecessary?)
-params <- data.frame(params, row.names = 1) # Change first column to row names
-#params <- sapply(file.list, extract.parameters) # R runs out of RAM quickly
-end_time <- Sys.time() # Measure execution time
-end_time - start_time # Display execution time
+features <- features[-1, ] # Remove redundant first row (unnecessary?)
+features <- na.omit(features) # Remove any missing observations
+features <- data.frame(features, row.names = 1) # Change first column to row names
 
-# k-means clustering of 29 parameters
-rescale_params <- params %>% mutate_all(scale) # scale all variables to ensure
-# weights equal so that it does not influence the clustering result
+### CLUSTER ANALYSIS ##
+
+# k-means clustering
+rescaled_features <- features %>% mutate_all(scale) # Standardize all variables
 
 # Optimizing k (elbow method)
 kmean_withinss <- function(k) { 
-  cluster <- kmeans(rescale_params[1:40, ], k) # Only try first 40 images
+  cluster <- kmeans(rescaled_features[1:40, ], k) # Only try first 40 images
   return(cluster$tot.withinss)
 }
-max_k <- 20 # Set maximum clusters 
-wss <- sapply(2:max_k, kmean_withinss) # Run algorithm over a range of k 
-elbow <- data.frame(2:max_k, wss) # Create a data frame to plot the graph
 
-# Plot graph of elbow method over 20 k-values
+max_k <- 20 # Set maximum clusters 
+wss <- sapply(2:max_k, kmean_withinss) # Run k-means over all k-values
+elbow <- data.frame(2:max_k, wss) # Create a data frame to plot elbow graph
+
+# Plot elbow method over 20 k-values
 ggplot(elbow, aes(x = X2.max_k, y = wss)) +
   labs(title = "Elbow method (total within-cluster sum of squares)",
        x = "k-value",
@@ -127,40 +129,23 @@ ggplot(elbow, aes(x = X2.max_k, y = wss)) +
   geom_line() +
   scale_x_continuous(breaks = seq(1, 20, by = 1))
 
-# # Silhouette method for determining optimal clusters (not working)
-# install.packages("cluster")
-# library(cluster)
-# avg_sil <- function(k) {
-#   km.res <- kmeans(rescale_params, centers = k, nstart = 25)
-#   ss <- silhouette(km.res$cluster, dist(rescale_params))
-#   mean(ss[, 3])
-# }
-# k.values <- 2:15 # Compute and plot wss for k = 2 to k = 15
-# avg_sil_values <- map_dbl(k.values, avg_sil)
-# plot(k.values, avg_sil_values,
-#      type = "b", pch = 19, frame = FALSE, 
-#      xlab = "Number of clusters K",
-#      ylab = "Average Silhouettes")
-# fviz_nbclust(df, kmeans, method = "silhouette")
-# 
-# # Gap statistic method for determining optimal clusters (not working)
-# library(NbClust)
-# gap_stat <- clusGap(rescale_params, FUN = kmeans, nstart = 25,
-#                     K.max = 20, B = 50)
-# fviz_gap_stat(gap_stat)
-
 # Train the k-means model with optimal k
-pc_cluster <- kmeans(rescale_params[1:40,], 7) # True k-value is 2 for 40 images
-#kmeans.ani(rescale_params, 4) # Train model with animations
+pc_cluster <- kmeans(rescaled_features[1:40,], 7) # True k is 2 for 40 images
 
 # Visualize clusters
 fviz_cluster(pc_cluster, # No labels
-             data = rescale_params,
+             data = rescaled_features,
              geom = "point",
              ellipse.type = "convex", 
              ggtheme = theme_bw(),
              main = "Cluster analysis of SAFARI output of 1400 images (k = 7)")
 
 fviz_cluster(pc_cluster, # Labels included
-             data = params[1:40,],
+             data = features[1:40,],
              main = "Cluster analysis of SAFARI output of 1400 images (k = 7)")
+
+# Hierarchical clustering
+
+# Gaussian mixture model clustering
+
+# Evaluation of clustering accuracy (adjusted Rand index)
