7fd982db7317958f72be0d05dbb3850d4a521840
kevinwjin <kwj200000@utdallas.edu>
2022-06-14 03:38:40 -0500

    Add Gaussian mixture model clustering and several plots

--- cluster_analysis.R
+++ cluster_analysis.R
@@ -89,7 +89,7 @@ features <- parSapply(cl, file_list, FUN = extract_features) # Parallelized vers
 stopCluster(cl) # De-allocate cores and expunge R sessions from memory
 
 # Clean up data frame
-features <- data.frame(t(features), row.names = 1) # Transpose result and redundant first column
+features <- data.frame(t(features), row.names = 1) # Transpose result and delete redundant first column
 features <- data.frame(lapply(features, unlist)) # Flatten column-lists to normal
 
 ##### CLUSTER ANALYSIS #####
@@ -141,27 +141,44 @@ plot(hcl, cex = 0.6, hang = -1) # Plot dendrogram
 hc2 <- agnes(rescaled_features, method = "complete") # Agnes function
 hc2$ac # Agglomerative coefficient
 
-# Gaussian mixture model clustering
+# Gaussian mixture model clustering using BIC
+gmm_scaled <- Mclust(rescaled_features, G = 70) # number of clusters
 
+# Optimal selected model
+gmm_scaled$modelName
+
+# Optimal number of clusters
+gmm_scaled$G
+
+# Probability for an observation to be in a given cluster
+head(gmm_scaled$z)
+
+# Classification matrix
+head(gmm_scaled$classification)
+
+# Probabilities, means, and variances
+summary(gmm_scaled, parameters = TRUE)
+
+# Visualize GMM clusters
+plot(gmm_scaled, what = c("classification"))
 
 ##### EVALUATE CLUSTERING ACCURACY #####
 
 # Calculate adjusted Rand index for each clustering method 
 max_k <- 100
-temp <- kmeans(x = rescaled_features, centers = 2)[[1]]
+temp <- kmeans(x = rescaled_features, centers = 2)[["cluster"]] # Get column names
 kmeans_mat <- matrix(nrow = 100, ncol = 1400, byrow = TRUE)
 
 # Generate k-means clustering from scaled features
 for (k in 1:max_k) {
-  kmeans_mat[k, ] <- kmeans(x = rescaled_features,  centers = k)[[1]]
+  kmeans_mat[k, ] <- kmeans(x = rescaled_features, centers = k)[["cluster"]]
 }
 kmeans_scaled <- as.data.frame(kmeans_mat)
 names(kmeans_scaled) <- names(temp)
-k_values <- 1:max_k
 
 # Generate k-means clustering from unscaled features
 for (k in 1:max_k) {
-  kmeans_mat[k, ] <- kmeans(x = features, centers = k)[[1]] # Select clustering results
+  kmeans_mat[k, ] <- kmeans(x = features, centers = k)[["cluster"]] # Select clustering results
 }
 kmeans_unscaled <- as.data.frame(kmeans_mat)
 names(kmeans_unscaled) <- names(temp)
@@ -173,15 +190,18 @@ kmeans_unscaled_truth <- kmeans(features, 70)[["cluster"]] # Unscaled truth
 kmeans_ARI <- matrix(nrow = 100, ncol = 2, byrow = TRUE)
 for (i in 1:max_k) {
   # ARI for k-means clustering of scaled features
-  kmeans_ARI[i, 1] <- adjustedRandIndex(kmeans_scaled[i, ], kmeans_scaled_truth)
+  kmeans_ARI[i, 1] <- adjustedRandIndex(unlist(kmeans_scaled[i, ]), 
+                                        kmeans_scaled_truth)
   # ARI for k-means clustering of unscaled features
-  kmeans_ARI[i, 2] <- adjustedRandIndex(kmeans_unscaled[i, ], kmeans_unscaled_truth)
+  kmeans_ARI[i, 2] <- adjustedRandIndex(unlist(kmeans_unscaled[i, ]), 
+                                        kmeans_unscaled_truth)
 }
 kmeans_ARI <- as.data.frame(kmeans_ARI)
 names(kmeans_ARI) <- c("ARI_scaled", "ARI_unscaled")
+k_values <- 1:max_k
 kmeans_ARI <- mutate(kmeans_ARI, k_values = as.numeric(row.names(kmeans_ARI)))
 
-# Visualize accuracy of k-means clustering
+# Visualize k-means clustering accuracy
 ggplot(kmeans_ARI, aes(x = k_values)) + 
   geom_point(aes(y = ARI_scaled, 
                  color = "darkred")) + 
@@ -192,38 +212,117 @@ ggplot(kmeans_ARI, aes(x = k_values)) +
   geom_line(aes(y = ARI_unscaled, 
                 color = "steelblue")) + 
   geom_vline(xintercept = 70, 
-             color = "red")
+             color = "red") +
   labs(title = "k-means clustering accuracy (ground truth: k = 70)",
        x = "k-value",
        y = "Adjusted Rand Index",
        color = "Features") +
   scale_color_hue(labels = c("Scaled", "Unscaled")) +
   scale_x_continuous(breaks = seq(0, 100, by = 10)) + 
-  scale_y_continuous(breaks = seq(0, 1, by = 0.1)) +
+  scale_y_continuous(breaks = seq(0, 1, by = 0.1))
 
 # Generate hierarchical clusters
-
+hier_scaled <- dist(rescaled_features, method = "euclidean") # Dissimilarity matrix
+hc1 <- hclust(d, method = "complete") # Complete linkage hierarchical clustering
+plot(hcl, cex = 0.6, hang = -1) # Plot dendrogram
 
 # Generate Gaussian mixture model clusters
+max_k <- 100
+temp <- Mclust(rescaled_features, G = 1) # Get column names
+gmm_mat <- matrix(nrow = 100, ncol = 1400, byrow = TRUE)
 
+# Generate GMM clustering from scaled features
+for (k in 1:max_k) {
+  gmm_mat[k, ] <- Mclust(rescaled_features, G = k)$classification
+}
+gmm_scaled <- as.data.frame(gmm_mat)
+names(gmm_scaled) <- row.names(rescaled_features)
 
-# Visualize accuracy of each clustering method
-ggplot(accuracy, aes(x = X2.max_k)) +
+# Generate GMM clustering from unscaled features
+for (k in 1:max_k) {
+  gmm_mat[k, ] <- Mclust(features, G = k)$classification
+}
+gmm_unscaled <- as.data.frame(gmm_mat)
+names(gmm_unscaled) <- row.names(features)
+
+# Calculate ARI values for GMM clustering over k = 1:100 (Ground truth: k = 70)
+gmm_scaled_truth <- Mclust(rescaled_features, G = 70)$classification # Scaled truth
+gmm_unscaled_truth <- Mclust(features, G = 70)$classification # Unscaled truth
+
+gmm_ARI <- matrix(nrow = 100, ncol = 2, byrow = TRUE)
+for (i in 1:max_k) {
+  # ARI for GMM clustering of scaled features
+  gmm_ARI[i, 1] <- adjustedRandIndex(unlist(gmm_scaled[i, ]), 
+                                     gmm_scaled_truth)
+  # ARI for GMM clustering of unscaled features
+  gmm_ARI[i, 2] <- adjustedRandIndex(unlist(gmm_unscaled[i, ]), 
+                                     gmm_unscaled_truth)
+}
+gmm_ARI <- as.data.frame(gmm_ARI)
+names(gmm_ARI) <- c("ARI_scaled", "ARI_unscaled")
+k_values <- 1:max_k
+gmm_ARI <- mutate(gmm_ARI, k_values = as.numeric(row.names(gmm_ARI)))
+
+# Visualize GMM clustering accuracy
+ggplot(gmm_ARI, aes(x = k_values)) + 
+  geom_point(aes(y = ARI_scaled, 
+                 color = "darkred")) + 
+  geom_line(aes(y = ARI_scaled, 
+                color = "darkred")) + 
+  geom_point(aes(y = ARI_unscaled, 
+                 color = "steelblue")) + 
+  geom_line(aes(y = ARI_unscaled, 
+                color = "steelblue")) + 
+  geom_vline(xintercept = 70, 
+             color = "red") +
+  labs(title = "GMM clustering accuracy (ground truth: k = 70)",
+       x = "k-value",
+       y = "Adjusted Rand Index",
+       color = "Features") +
+  scale_color_hue(labels = c("Scaled", "Unscaled")) +
+  scale_x_continuous(breaks = seq(0, 100, by = 10)) + 
+  scale_y_continuous(breaks = seq(0, 1, by = 0.1))
+
+# Visualize accuracy of all clustering methods
+accuracy <- right_join(kmeans_ARI, gmm_ARI, by = "k_values")
+names(accuracy) <- c("kmeans_ARI_scaled", 
+                     "kmeans_ARI_unscaled", 
+                     "k_values",
+                     "gmm_ARI_scaled",
+                     "gmm_ARI_unscaled")
+
+ggplot(accuracy, aes(x = k_values)) +
+  #geom_point(aes(y = kmeans_ARI_scaled,
+  #              color = "darkred")) +
+  geom_line(aes(y = kmeans_ARI_scaled,
+                color = "darkred",
+                linetype = "solid")) +
+  #geom_point(aes(y = kmeans_ARI_unscaled,
+  #              color = "darkred")) +
+  geom_line(aes(y = kmeans_ARI_unscaled,
+                color = "darkred",
+                linetype = "twodash")) +
+  #geom_line(aes(y = ARI_hierarchical_scaled), 
+  #          color = "steelblue") +
+  #geom_line(aes(y = ARI_hierarchical_unscaled), 
+  #          color = "steelblue", 
+  #          linetype = "twodash") +
+  #geom_point(aes(y = gmm_ARI_scaled,
+  #                color = "seagreen")) +
+  geom_line(aes(y = gmm_ARI_scaled,
+                color = "seagreen",
+                linetype = "solid")) +
+  #geom_point(aes(y = gmm_ARI_unscaled,
+  #                color = "seagreen")) +
+  geom_line(aes(y = gmm_ARI_unscaled,
+                color = "seagreen", 
+                linetype = "twodash")) +
+  geom_vline(xintercept = 70, 
+             color = "red") +
   labs(title = "Performance Metrics of Several Clustering Methods",
        x = "Number of clusters (k-value)",
        y = "Adjusted Rand Index") +
-  geom_line(aes(y = ARI_kmeans_scaled), 
-            color = "darkred") +
-  geom_line(aes(y = ARI_kmeans_unscaled), 
-            color = "darkred", 
-            linetype = "twodash") +
-  geom_line(aes(y = ARI_hierarchical_scaled), 
-            color = "steelblue") +
-  geom_line(aes(y = ARI_hierarchical_unscaled), 
-            color = "steelblue", 
-            linetype = "twodash") +
-  geom_line(aes(y = ARI_gaussian_scaled), 
-            color = "seagreen") +
-  geom_line(aes(y = ARI_gaussian_unscaled), 
-            color = "seagreen", 
-            linetype = "twodash")
+  scale_x_continuous(breaks = seq(0, 100, by = 10)) + 
+  scale_y_continuous(breaks = seq(0, 1, by = 0.1)) +
+  scale_color_hue(labels = c("k-means", "GMM")) +
+  scale_linetype(labels = c("Scaled", "Unscaled"))
